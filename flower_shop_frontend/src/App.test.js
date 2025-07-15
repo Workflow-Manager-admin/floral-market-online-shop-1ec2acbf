179c9f96c632e7e4952e285237af48911f69ca85
Kavia AI <production@kavia.ai>
2025-07-15 15:56:07 +0000

    CheckPoint - cg895cac5c

--- flower_shop_frontend/src/App.test.js
+++ flower_shop_frontend/src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+// PUBLIC_INTERFACE
+test('renders login or shop', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const textInDoc = screen.getByText(/floral\.market/i);
+  expect(textInDoc).toBeInTheDocument();
 });
